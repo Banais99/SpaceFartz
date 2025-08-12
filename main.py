3f4d133d7be737f557451691bc7d8d53696f80f8
Banais99 <p.soteriou@protonmail.com>
2025-08-12 16:21:31 +0300

    adding triangle object , started the movement

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import pygame
 from constants import *
-
+from player import *
 
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock=pygame.time.Clock()
+    dt = 0
+    player=Player(SCREEN_WIDTH / 2 ,SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = clock.tick(60) /1000
+            
+
 
 
 if __name__ == "__main__":
